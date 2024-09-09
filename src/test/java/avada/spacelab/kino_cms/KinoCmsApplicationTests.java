d593116f0dd41f6806ac226ce427fc515f0abe7b
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-09 16:52:53 +0300

    test files

---
+++ src/test/java/avada/spacelab/kino_cms/KinoCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package avada.spacelab.kino_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
