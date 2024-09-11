c4078f09a3ab4717752bdb1214460f017395cc24
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-11 13:15:46 +0300

    add findById method to TheaterService

--- src/main/java/avada/spacelab/kino_cms/service/TheaterService.java
+++ src/main/java/avada/spacelab/kino_cms/service/TheaterService.java
@@ -1,16 +1,22 @@
 package avada.spacelab.kino_cms.service;
 
+import avada.spacelab.kino_cms.model.dto.TheaterDto;
+import avada.spacelab.kino_cms.model.mapper.TheaterMapper;
 import avada.spacelab.kino_cms.repository.TheaterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TheaterService {
-    private TheaterRepository theaterRepository;
+    private final TheaterRepository theaterRepository;
 
     public TheaterService(
             @Autowired TheaterRepository theaterRepository
     ) {
         this.theaterRepository = theaterRepository;
+    }
+
+    public TheaterDto findTheaterById(long id) {
+        return TheaterMapper.INSTANCE.fromEntityToDto(theaterRepository.findTheaterById(id));
     }
 }
