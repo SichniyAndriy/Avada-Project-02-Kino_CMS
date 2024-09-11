a9d036fbb05c602436a35b047f5f9a52971aa51f
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-11 14:15:55 +0300

    add getAll methods

--- src/main/java/avada/spacelab/kino_cms/service/MovieService.java
+++ src/main/java/avada/spacelab/kino_cms/service/MovieService.java
@@ -3,6 +3,9 @@ package avada.spacelab.kino_cms.service;
 import avada.spacelab.kino_cms.model.dto.MovieDto;
 import avada.spacelab.kino_cms.model.mapper.MovieMapper;
 import avada.spacelab.kino_cms.repository.MovieRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,12 @@ public class MovieService {
             @Autowired MovieRepository movieRepository
     ) {
         this.movieRepository = movieRepository;
+    }
+
+    public List<MovieDto> getAllMovies() {
+        return movieRepository.findAll().stream()
+                .map(MovieMapper.INSTANCE::fromEntityToDto)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     public MovieDto getMovieById(long id) {
--- src/main/java/avada/spacelab/kino_cms/service/TheaterService.java
+++ src/main/java/avada/spacelab/kino_cms/service/TheaterService.java
@@ -3,6 +3,9 @@ package avada.spacelab.kino_cms.service;
 import avada.spacelab.kino_cms.model.dto.TheaterDto;
 import avada.spacelab.kino_cms.model.mapper.TheaterMapper;
 import avada.spacelab.kino_cms.repository.TheaterRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,12 @@ public class TheaterService {
             @Autowired TheaterRepository theaterRepository
     ) {
         this.theaterRepository = theaterRepository;
+    }
+
+    public List<TheaterDto> getAllTheaters() {
+        return theaterRepository.findAll().stream()
+                .map(TheaterMapper.INSTANCE::fromEntityToDto)
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     public TheaterDto findTheaterById(long id) {
