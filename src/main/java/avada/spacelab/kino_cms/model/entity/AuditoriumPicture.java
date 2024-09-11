c29577eaa28410aad76ec62ca87bd0f8a8063a79
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-11 11:48:16 +0300

    add Auditorium picture entity

---
+++ src/main/java/avada/spacelab/kino_cms/model/entity/AuditoriumPicture.java
@@ -0,0 +1,34 @@
+package avada.spacelab.kino_cms.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Getter @Setter @NoArgsConstructor
+@Entity  @Table(name = "auditorium_pictures")
+public class AuditoriumPicture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "auditorium_picture_gen")
+    @SequenceGenerator(name = "auditorium_picture_gen", sequenceName = "auditorium_picture_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    @JdbcTypeCode(SqlTypes.BIGINT)
+    private Long id;
+
+    @Column(name = "path", nullable = false, length = 512, unique = true)
+    private String path;
+
+    @ManyToOne
+    @JoinColumn(name = "auditorium_id", referencedColumnName = "id", nullable = false)
+    private Auditorium auditorium;
+}
