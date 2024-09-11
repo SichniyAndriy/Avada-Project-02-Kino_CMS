acfd4941d7b633c26eab3977a7007cf935a28394
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-11 13:15:18 +0300

    add constructor to AuditoriumDto

--- src/main/java/avada/spacelab/kino_cms/model/dto/AuditoriumDto.java
+++ src/main/java/avada/spacelab/kino_cms/model/dto/AuditoriumDto.java
@@ -13,4 +13,30 @@ public record AuditoriumDto(
         String mainBannerUrl,
         SeoBlockDto seoBlock
 ) implements Serializable {
+    public AuditoriumDto(
+            Long id,
+            Integer number,
+            String schemeUrl,
+            String description,
+            String mainBannerUrl,
+            SeoBlockDto seoBlock
+    ) {
+        this.id = id;
+        this.number = number;
+        this.schemeUrl = schemeUrl;
+        this.description = description;
+        this.mainBannerUrl = mainBannerUrl;
+        this.seoBlock = seoBlock;
+    }
+
+    public AuditoriumDto() {
+        this(
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+    }
 }
