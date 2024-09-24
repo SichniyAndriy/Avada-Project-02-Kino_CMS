ad0e8e65abc63dfecdd75fda7967fcce577e141e
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-24 18:43:16 +0300

    add util class for controllers

---
+++ src/main/java/avada/spacelab/kino_cms/controller/util/ControllerUtil.java
@@ -0,0 +1,53 @@
+package avada.spacelab.kino_cms.controller.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.springframework.web.multipart.MultipartFile;
+
+public class ControllerUtil {
+    public static String savePictureOnServer(
+            String serverPath,
+            String fileName,
+            String timestamp,
+            String type,
+            MultipartFile file
+    ) throws IOException {
+        File dir = new File(serverPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        for (File fileEntry : dir.listFiles()) {
+            if (fileEntry.getName().contains(fileName)) {
+                fileEntry.delete();
+            }
+        }
+        String filePath = File.separator +
+                serverPath + File.separator +
+                fileName + File.separator +
+                timestamp + "." + type;
+        Files.write(Path.of(filePath), file.getBytes());
+        return filePath;
+    }
+
+    public static String saveEmailOnServer(
+            String serverPath,
+            String fileName,
+            String type,
+            byte[] file
+    ) throws IOException {
+        File dir = new File(serverPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        for (File fileEntry : dir.listFiles()) {
+            if (fileEntry.getName().contains(fileName)) {
+                fileEntry.delete();
+            }
+        }
+        String filePath = serverPath + fileName + "." + type;
+        Files.write(Path.of(filePath), file);
+        return filePath;
+    }
+}
