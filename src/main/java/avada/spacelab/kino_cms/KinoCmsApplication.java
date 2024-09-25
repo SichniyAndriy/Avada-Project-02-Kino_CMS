b65ae389d9224ca8743ccfb6329e7dc5e0b81841
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-25 14:20:15 +0300

    add async in main

--- src/main/java/avada/spacelab/kino_cms/KinoCmsApplication.java
+++ src/main/java/avada/spacelab/kino_cms/KinoCmsApplication.java
@@ -2,12 +2,12 @@ package avada.spacelab.kino_cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class KinoCmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KinoCmsApplication.class, args);
 	}
-
 }
