d90f28b927584eaf3723c83ec08e15f23c02c015
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-09-25 14:18:48 +0300

    change sendEmail method

--- src/main/java/avada/spacelab/kino_cms/controller/util/ControllerUtil.java
+++ src/main/java/avada/spacelab/kino_cms/controller/util/ControllerUtil.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Instant;
 import java.util.Objects;
 import org.springframework.web.multipart.MultipartFile;
 
 public class ControllerUtil {
+    public static final String PATH_TO_SENT_EMAIL = "src/main/resources/service_files/sent_emails";
+
     public static String savePictureOnServer(
             String serverPath,
             String fileName,
@@ -33,21 +36,33 @@ public class ControllerUtil {
     }
 
     public static String saveEmailOnServer(
-            String serverPath,
             String fileName,
             String type,
             byte[] file
     ) throws IOException {
-        File dir = new File(serverPath);
+        File dir = new File(PATH_TO_SENT_EMAIL);
         if (!dir.exists()) {
             dir.mkdirs();
         }
+
+        File olderFile = null;
+        Instant older = Instant.now();
+        int amount = 0;
         for (File fileEntry : Objects.requireNonNull(dir.listFiles())) {
+            ++amount;
+            if (fileEntry.lastModified() < older.toEpochMilli()) {
+                older = Instant.ofEpochMilli(fileEntry.lastModified());
+                olderFile = fileEntry;
+            }
             if (fileEntry.getName().contains(fileName)) {
                 fileEntry.delete();
             }
         }
-        String filePath = serverPath + fileName + "." + type;
+        if (amount > 4) {
+            assert olderFile != null;
+            olderFile.delete();
+        }
+        String filePath = PATH_TO_SENT_EMAIL + "/" + fileName + "." + type;
         Files.write(Path.of(filePath), file);
         return filePath;
     }
