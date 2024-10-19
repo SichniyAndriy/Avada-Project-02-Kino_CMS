f3b3efd3daae7fa66c5e5daeaaacf3c43c623557
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-10-19 18:44:54 +0300

    fixing resources

--- src/main/resources/static/js/admin/movies-scripts.js
+++ src/main/resources/static/js/admin/movies-scripts.js
@@ -12,23 +12,23 @@ function showMovie(id=0) {
 }
 
 function loadPictures() {
-    const loadLazy = (entries, observer) => {
-        entries.forEach(entrie => {
-            if (entrie.isIntersecting) {
-                let img = entrie.target;
-                img.src = img.dataset.src;
-                img.classList.remove("lazy");
-                img.removeAttribute("data-src");
-                observer.unobserve(img);
+    const loadLazy = function(entries, observer) {
+        for (const entry of entries) {
+            if (entry.isIntersecting) {
+                const $img = $(entry.target);
+                const path = $img.data("src");
+                $img.attr("src", path);
+                $img.removeData("src").removeClass("lazy");
+                observer.unobserve($img[0]);
             }
-        });
+        }
     }
 
     const observer = new IntersectionObserver(loadLazy, {
         root: null,
         rootMargin: "0px",
         threshold: 0.1
-    })
+    });
 
     $("img.lazy").each((i, img) => observer.observe(img));
 }
