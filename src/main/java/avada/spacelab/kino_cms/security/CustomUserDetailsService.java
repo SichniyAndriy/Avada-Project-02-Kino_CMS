6932171af6bf7233b70a1e997d620fb0598c941e
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-11-15 12:05:49 +0200

    add Custom UserDetailsService

---
+++ src/main/java/avada/spacelab/kino_cms/security/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package avada.spacelab.kino_cms.security;
+
+import avada.spacelab.kino_cms.model.entity.User;
+import avada.spacelab.kino_cms.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(
+            @Autowired UserRepository userRepository
+    ) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            List<GrantedAuthority> authorities = user.getRole().getPermissions(user.getRole()).stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .map(GrantedAuthority.class::cast)
+                    .toList();
+            return new org.springframework.security.core.userdetails.User(
+                    user.getEmail(),
+                    user.getPassHash(),
+                    authorities
+            );
+        }
+        throw new UsernameNotFoundException(username);
+    }
+}
