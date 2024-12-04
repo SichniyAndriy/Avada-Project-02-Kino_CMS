3bb12bc2853259243572555d54bce1f68d1ac668
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-12-04 06:45:52 +0200

    fixes syntax in security

--- src/main/java/avada/spacelab/kino_cms/security/CustomUserDetailsService.java
+++ src/main/java/avada/spacelab/kino_cms/security/CustomUserDetailsService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
+
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(
